596b694180693575d7bd02926e11f179e603d644
Arun K. Prajapati <arunkp1122@gmail.com>
2020-08-09 18:26:43 +0545

    [Deploy] Deployment ready

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const volleyball = require("volleyball");
 const cookieParser = require("cookie-parser");
 const cookieSession = require("cookie-session");
 const cors = require("cors");
+const path = require("path");
 
 const connect = require("./config/DBConnect");
 const log = require("./utils/logHandler");
@@ -20,6 +21,13 @@ dotenv.config({
 
 connect();
 
+if (process.env.NODE_ENV === "production") {
+   app.use(express.static("client/build"));
+   app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+   });
+}
+
 //MIDDLEWARES
 // @DESC         Body-parser
 app.all("/*", function (req, res, next) {
@@ -27,7 +35,11 @@ app.all("/*", function (req, res, next) {
    next();
 });
 app.use(express.json());
-app.use(volleyball);
+
+if (process.env.NODE_ENV !== "production") {
+   app.use(volleyball);
+}
+
 app.use(cookieParser());
 app.use(cors());
 // app.options("*", cors());
