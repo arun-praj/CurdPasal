95728b1c79817a5a7a514ae6c744652859d86bca
Arun K. Prajapati <arunkp1122@gmail.com>
2020-08-11 22:57:21 +0545

    [Update] class based to functional component

--- client/src/components/Products/Product.js
+++ client/src/components/Products/Product.js
@@ -5,37 +5,36 @@ import { loadProducts } from "../../redux/action/product";
 
 import Card from "./Card/Card";
 
-class Product extends Component {
-   componentDidMount() {
-      this.props.loadProducts();
-   }
-
-   render() {
-      let val;
-
-      return <> Leage of legends</>;
-   }
-}
-// const Product = (props) => {
-//    const [prodcut, setProducts] = useState([]);
-//    const { products } = props;
-//    useEffect(() => {
-//       props.loadProducts();
-
-//       setProducts((prev) => [...prev, products]);
-//    }, []);
-//    // console.log(props.products);
-//    let val;
-//    if (props.loading) {
-//       val = <div>Loading</div>;
-//    } else {
-//       val = props.products.map((el) => {
-//          return <Card key={el._id} data={el} />;
-//       });
+// class Product extends Component {
+//    componentDidMount() {
+//       this.props.loadProducts();
 //    }
-//    return <div>{val}</div>;
-//    //    return <div>{props.loading === true ? : <div>product.map()</div>}</div>;
-// };
+
+//    render() {
+//       let val;
+
+//       return <> Leage of legends</>;
+//    }
+// }
+const Product = (props) => {
+   const [prodcut, setProducts] = useState([]);
+   const { products } = props;
+   useEffect(() => {
+      props.loadProducts();
+      setProducts((prev) => [...prev, products]);
+   }, []);
+   // console.log(props.products);
+   let val;
+   if (props.loading) {
+      val = <div>Loading</div>;
+   } else {
+      val = props.products.map((el) => {
+         return <Card key={el._id} data={el} />;
+      });
+   }
+   return <div>{val}</div>;
+   //    return <div>{props.loading === true ? : <div>product.map()</div>}</div>;
+};
 
 const mapStateToProps = (state) => ({
    products: state.product.products,
