2a68b1f2d313588f2565dd2613d1467018e3c4a1
Arun K. Prajapati <arunkp1122@gmail.com>
2020-08-11 20:02:02 +0545

    test

--- client/src/components/Products/Product.js
+++ client/src/components/Products/Product.js
@@ -1,28 +1,47 @@
-import React, { useEffect, useState } from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 
 import { loadProducts } from "../../redux/action/product";
 
 import Card from "./Card/Card";
-const Product = (props) => {
-   const [prodcut, setProducts] = useState([]);
-   const { products } = props;
-   useEffect(() => {
-      props.loadProducts();
-      setProducts((prev) => [...prev, products]);
-   }, []);
-   // console.log(props.products);
-   let val;
-   if (props.loading) {
-      val = <div>Loading</div>;
-   } else {
-      val = props.products.map((el) => {
-         return <Card key={el._id} data={el} />;
-      });
+
+class Product extends Component {
+   componentDidMount() {
+      this.props.loadProducts();
    }
-   return <div>{val}</div>;
-   //    return <div>{props.loading === true ? : <div>product.map()</div>}</div>;
-};
+
+   render() {
+      let val;
+      if (this.props.loading) {
+         val = <div>Loading</div>;
+      } else {
+         val = this.props.products.map((el) => {
+            return <Card key={el._id} data={el} />;
+         });
+      }
+      return <> {val}</>;
+   }
+}
+// const Product = (props) => {
+//    const [prodcut, setProducts] = useState([]);
+//    const { products } = props;
+//    useEffect(() => {
+//       props.loadProducts();
+
+//       setProducts((prev) => [...prev, products]);
+//    }, []);
+//    // console.log(props.products);
+//    let val;
+//    if (props.loading) {
+//       val = <div>Loading</div>;
+//    } else {
+//       val = props.products.map((el) => {
+//          return <Card key={el._id} data={el} />;
+//       });
+//    }
+//    return <div>{val}</div>;
+//    //    return <div>{props.loading === true ? : <div>product.map()</div>}</div>;
+// };
 
 const mapStateToProps = (state) => ({
    products: state.product.products,
