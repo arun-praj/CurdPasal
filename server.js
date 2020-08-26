1a4654b91bff3d831c34c9386c1f9ef002dda4cc
Arun K. Prajapati <arunkp1122@gmail.com>
2020-08-26 09:15:48 +0545

    Deploy test

--- server.js
+++ server.js
@@ -31,13 +31,6 @@ connect();
 //    app.use(proxy(["/api"], { target: "http://localhost:8000" }));
 // };
 
-if (process.env.NODE_ENV === "production") {
-   app.use(express.static("client/build"));
-   app.get("/", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-   });
-}
-
 //MIDDLEWARES
 app.use(express.json());
 app.use(express.static("public"));
@@ -64,6 +57,12 @@ app.use(
       keys: [process.env.COOKIE_KEY],
    })
 );
+if (process.env.NODE_ENV === "production") {
+   app.use(express.static("client/build"));
+   app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+   });
+}
 
 //Routes
 app.use("/api/products", require("./routes/products"));
